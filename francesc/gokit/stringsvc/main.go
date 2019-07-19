a977b9c16630b4975683ab3a9bc076c202cddca6
Francesc Campoy <francesc@dgraph.io>
2019-07-19 14:58:36 -0700

    added logging with middleware

--- francesc/gokit/stringsvc/main.go
+++ francesc/gokit/stringsvc/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
@@ -13,21 +16,37 @@ func main() {
 	port := flag.Int("p", 8080, "port on which the server should listen")
 	flag.Parse()
 
+	logger := log.NewLogfmtLogger(os.Stderr)
+
 	svc := stringService{}
 
-	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
+	var uppercase endpoint.Endpoint
+	uppercase = makeUppercaseEndpoint(svc)
+	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
+	http.Handle("/uppercase", httptransport.NewServer(
+		uppercase,
 		decodeUppercaseRequest,
 		encodeResponse,
-	)
+	))
 
-	countHandler := httptransport.NewServer(
-		makeCountEndpoint(svc),
+	var count endpoint.Endpoint
+	count = makeCountEndpoint(svc)
+	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
+	http.Handle("/count", httptransport.NewServer(
+		count,
 		decodeCountRequest,
 		encodeResponse,
-	)
+	))
 
-	http.Handle("/uppercase", uppercaseHandler)
-	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
+}
+
+func loggingMiddleware(logger log.Logger) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (interface{}, error) {
+			logger.Log("msg", "calling endpoint")
+			defer logger.Log("msg", "called endpoint")
+			return next(ctx, request)
+		}
+	}
 }
